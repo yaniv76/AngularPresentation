b0fcdde6398c901caf62f9c867bc85097f69eebe
Yaniv.Bogdanov <yaniv.bogdanov@borderfree.com>
2015-11-03 18:58:39 +0200

    editable list

--- app/scripts/controllers/sandbox.js
+++ app/scripts/controllers/sandbox.js
@@ -11,18 +11,24 @@ angularPresentationApp.controller('SandboxCtrl', function ($scope) {
 
   window.scope = $scope;
 
-    $scope.listOfWorkersInBorderfree = [
-      {first:'Anatoly', last: 'Libman'},
-      {first:'Avi', last: 'TooMany'},
-      {first:'Avishay', last: 'Oshri'},
-      {first:'Dekel', last: 'Moyal'},
-      {first:'Denis', last: 'Denis'},
-      {first:'Dmitry', last: 'Kar'},
-      {first:'Eyal', last: 'AlsoTooMany'},
-      {first:'Haim', last: 'Sulam'},
-      {first:'Genady', last: 'Kof'}
-    ];
+  $scope.listOfWorkersInBorderfree = [
+    {first: 'Anatoly', last: 'Libman'},
+    {first: 'Avi', last: 'TooMany'},
+    {first: 'Avishay', last: 'Oshri'},
+    {first: 'Dekel', last: 'Moyal'},
+    {first: 'Denis', last: 'Denis'},
+    {first: 'Dmitry', last: 'Kar'},
+    {first: 'Eyal', last: 'AlsoTooMany'},
+    {first: 'Haim', last: 'Sulam'},
+    {first: 'Genady', last: 'Kof'}
+  ];
 
+  $scope.newWorker = "";
 
+  $scope.addWorker = function () {
+    var worker = $scope.newWorker.split(',');
+    $scope.listOfWorkersInBorderfree.push({first:worker[0], last:worker[1]});
+    $scope.newWorker = "";
+  }
 
-  });
+});
